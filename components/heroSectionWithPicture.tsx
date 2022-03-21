03d479d63bdc7a7036ba0930c02af56550eaa0a4
jordi <jordi.vilagut@andjoy.life>
2022-03-21 17:08:25 +0100

    Add new hero section with picture

---
+++ components/heroSectionWithPicture.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Home.module.css";
+import {SectionModel} from "../model/SectionModel";
+
+interface HeroSectionProps {
+    section: SectionModel
+}
+
+export const HeroSectionWithPicture = (props: HeroSectionProps) =>
+    <section id={props.section.id} className={props.section.styles.join(' ')}>
+        <div className="stainedGlass">
+            <h1 className={`${styles.title} ${styles.topPadding}`}>{props.section.title}</h1>
+            <p className={styles.description}>{props.section.text}</p>
+        </div>
+    </section>;
